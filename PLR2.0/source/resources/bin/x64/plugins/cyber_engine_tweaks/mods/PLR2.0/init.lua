469362e39897a9f4ce7bae2f8c49cc1f197d16d1
Riviera Sperduto <52908660+rivie13@users.noreply.github.com>
2023-07-19 08:53:37 -0400

    2.1 update
    This update comments out some code that is causing a bug. This will make it so there are no quality restrictions for the launcher parts anymore while I investigate what is causing the bug with weapon mods people are having.

--- PLR2.0/source/resources/bin/x64/plugins/cyber_engine_tweaks/mods/PLR2.0/init.lua
+++ PLR2.0/source/resources/bin/x64/plugins/cyber_engine_tweaks/mods/PLR2.0/init.lua
@@ -35,26 +35,30 @@ function PLR:new()
     This code below was kindly provided by psiberx. I would like to thank them for all their help and teaching
     that they have been provided to me. I honestly have no idea how it really works but I will try my best to describe it.
 ]]
+
+--[[
     IsGreaterQuality = Game['OperatorGreater;gamedataQualitygamedataQuality;Bool']
 
     Override('InventoryDataManagerV2', 'GetPlayerInventoryPartsForItem;ItemIDTweakDBID', function(this, itemID, slotID, wrapped)
-        local parts = wrapped(itemID, slotID) -- took out slotID... it needs 
+        local parts = wrapped(itemID, slotID)
         local item = TweakDB:GetRecord(itemID:GetTDBID())
         if item:ItemType():Type() == gamedataItemType.Cyb_Launcher then
             local itemData = this.TransactionSystem:GetItemData(this.Player, itemID)
             local itemQuality = RPGManager.GetItemDataQuality(itemData)
             for _, part in ipairs(parts) do
-                local partQuality = InventoryItemData.GetComparedQuality(part)
-                if IsGreaterQuality(partQuality, itemQuality) then
-                    part.IsRequirementMet = false
+                if InventoryItemData.GetItemType(part) == gamedataItemType.Prt_Fragment then
+                    local partQuality = InventoryItemData.GetComparedQuality(part)
+                    if IsGreaterQuality(partQuality, itemQuality) then
+                        part.IsRequirementMet = false
+                        print("part lower quality")
+                    end
                 end
             end
         end
         return parts
     end)
 
-
-
+]]
 
 	print("Projectile Launcher Rework fully loaded!!")	
     end)
